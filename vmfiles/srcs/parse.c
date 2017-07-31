576f63544fadca71a1d2407c11ab07f8ed1b69ec
Thibaut FONTAINE <tfontain@e2r9p5.42.fr>
2017-07-31 13:37:01 +0200

    norme ending fix

--- vmfiles/srcs/parse.c
+++ vmfiles/srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <tfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 20:35:26 by tfontain          #+#    #+#             */
-/*   Updated: 2017/07/31 11:19:26 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/07/31 13:36:30 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,28 @@ static inline char		*to_memory(const char *arg,
 	return (arena);
 }
 
-/*static inline void		case_flag(int *ac, const char *av[], t_argv *ret,
+static inline void		case_flag(int *ac, const char **av[], t_argv *ret,
 		int *n)
 {
-	if ((*av)[1] == 'n' || (*av)[1] == 's' || (*av)[1] == 'd')
+	if ((**av)[1] == 'n' || (**av)[1] == 's' || (**av)[1] == 'd')
 	{
-		if ((*av)[1] == 'n')
-			*n = ft_atoi(av[1]);
+		if ((**av)[1] == 'n')
+			*n = ft_atoi((*av)[1]);
 		else
 		{
 			if (ret->f.n != 0)
 				error(_ERR_USAGE)();
-			ret->f.flag |= ((*av)[1] == 'd' ? _D_ : _S_);
-			ret->f.n = ft_atoi(av[1]);
+			ret->f.flag |= ((**av)[1] == 'd' ? _D_ : _S_);
+			ret->f.n = ft_atoi((*av)[1]);
 		}
 		--*ac;
-		++av;
+		++*av;
 	}
-	else if ((*av)[1] == 'g')
+	else if ((**av)[1] == 'g')
 		ret->f.flag |= _G_;
-}*/
+}
 
-/*static inline void			case_player(int *n, t_argv ret, const char *av[],
+static inline void		case_player(int *n, t_argv ret, const char *av[],
 		const char *champs[])
 {
 	if (*n != -1)
@@ -59,7 +59,19 @@ static inline char		*to_memory(const char *arg,
 		champs[*n] = *av;
 		*n = -1;
 	}
-}*/
+}
+
+static inline void		load_memory(t_argv *ret, const char *champs[])
+{
+	int		n;
+
+	n = 0;
+	while (n < ret->n_champs)
+	{
+		ret->arena = to_memory(champs[n], ret->champ, ret->n_champs, n);
+		++n;
+	}
+}
 
 t_argv					*parse(int ac, const char *av[])
 {
@@ -77,52 +89,13 @@ t_argv					*parse(int ac, const char *av[])
 	while (ac > 0)
 	{
 		if ((*av)[0] == '-' && (*av)[2] == 0)
-		{
-			//case_flag(&ac, av, &ret, &n);
-			if ((*av)[1] == 'n' || (*av)[1] == 's' || (*av)[1] == 'd')
-			{
-				if ((*av)[1] == 'n')
-					n = ft_atoi(av[1]);
-				else
-				{
-					if (ret.f.n != 0)
-						error(_ERR_USAGE)();
-					ret.f.flag |= ((*av)[1] == 'd' ? _D_ : _S_);
-					ret.f.n = ft_atoi(av[1]);
-				}
-				--ac;
-				++av;
-			}
-			else if ((*av)[1] == 'g')
-				ret.f.flag |= _G_;
-		}
+			case_flag(&ac, &av, &ret, &n);
 		else
-		{
-			//case_player(&n, ret, av, champs);
-			if (n != -1)
-			{
-				if (n > ret.n_champs || champs[n - 1] != *av)
-					error(_ERR_BAD_NFLAG)();
-				n = -1;
-			}
-			else
-			{
-				n = 0;
-				while (champs[n] != NULL)
-					++n;
-				champs[n] = *av;
-				n = -1;
-			}
-		}
+			case_player(&n, ret, av, champs);
 		++av;
 		--ac;
 	}
-	n = 0;
-	while (n < ret.n_champs)
-	{
-		ret.arena = to_memory(champs[n], ret.champ, ret.n_champs, n);
-		++n;
-	}
+	load_memory(&ret, champs);
 	if (ret.f.flag & _G_)
 		ret.color = init_color_arena(&ret);
 	ret.ref_tab = get_ref_tab();
